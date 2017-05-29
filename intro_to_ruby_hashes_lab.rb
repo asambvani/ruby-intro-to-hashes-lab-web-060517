80843b85a096414a82208750d54d8330160ec708
Alex Sambvani <alexsambvani@gmail.com>
2017-05-29 14:08:15 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,31 +1,44 @@
 def new_hash
-
+  hash = {}
+  return hash
 end
 
 def actor
-
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :names => {},
+      :rent_in_dollars => {}
+      }
+    }
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+  third_tier = monopoly_with_second_tier
+	third_tier[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  third_tier[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  third_tier[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  third_tier[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  third_tier[:railroads][:names][:reading_railroad] = {}
+  third_tier[:railroads][:names][:pennsylvania_railroad] = {}
+  third_tier[:railroads][:names][:b_and_o_railroad] = {}
+  third_tier[:railroads][:names][:shortline] = {}
+  return third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+	fourth_tier = monopoly_with_third_tier
+  fourth_tier[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+  fourth_tier[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
+  fourth_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+  fourth_tier[:railroads][:names][:shortline]["mortgage_value"] = "$800"
+  return fourth_tier
 end
-
-
-
-
